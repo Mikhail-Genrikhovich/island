9149abb23c865537fdc52c64a6565da21b93e332
mikhail <mikhail0096@gmail.com>
2026-02-14 16:17:45 +0300

    initialization project

---
+++ src/main/java/com/javarush/mikhail/application/IslandApplication.java
@@ -0,0 +1,7 @@
+package com.javarush.mikhail.application;
+
+public class IslandApplication {
+    public static void main(String[] args) {
+        System.out.println("Симуляция острова запущена");
+    }
+}
